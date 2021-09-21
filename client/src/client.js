b90d3e53a5a152056e36a628c3428f4eb443ce8f
soumyapal96 <pal.soumya1996@gmail.com>
2021-09-21 09:43:10 -0400

    update

--- client/src/client.js
+++ client/src/client.js
@@ -75,35 +75,57 @@ await storage.shareViaPublicKey({
 function App() {
   const [usernameReg, setUsernameReg] = useState('')
   const [passwordReg, setPasswordReg] = useState('')
+
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const[loginStatus, setLoginStatus]= useState("")
+
   const register = () => {
     Axios.post('http://localhost:5000/register', {
-      username:usernameReg, password:passwordReg}).then((response) => {console.log(response);});
+      username:usernameReg, password:passwordReg}).then((response) => {console.log(response.data);});
+  };
+
+  const login = () => {
+    Axios.post('http://localhost:5000/login', {
+      username:username, password:password}).then((response) => {
+        if(response.data.message) {
+          setLoginStatus(response.data.message);
+        } else {
+          setLoginStatus(response.data[0].username);
+        }
+      });
   };
 
   return (
     <div className="App">
         <div className="Registration">
-          <h1>Registration</h1>
+          <h1>Registration</h1> <br/>
           <label>Username</label>
           <input type='text' onChange={(e) => {
             setUsernameReg(e.target.value);
-          }}/>
+          }}/><br/>
           <label>Password</label>
           <input type='text' onChange={(e) => {
             setPasswordReg(e.target.value);
-          }}/>
-          <button onClick = {register}>Register</button>
+          }}/><br/>
+          <button onClick = {register}>Register</button><br/><br/>
         </div>
 
         <div className="Regislogintration">
-          <h1>Login</h1>
-          <input type='text' placeholder='Username' />
-          <input type='password' placeholder='password' />
-          <button>Register</button>
+          <h1>Login</h1><br/>
+          <input type='text' placeholder='Username' onChange={(e) => {
+            setUsername(e.target.value);
+          }} /><br/>
+          <input type='password' placeholder='password' onChange={(e) => {
+            setPassword(e.target.value);
+          }}/><br/>
+          <button onClick = {login}>Login</button><br/>
         </div>
-        <button className="createToken" onClick = {userCreate}> Create User</button>
-        <button className="createToken" onClick = {createStorage}> Create Storage</button>
-        <button className="createToken" onClick = {fileShare}> Share File</button>
+        <button className="createToken" onClick = {userCreate}> Create User</button><br/>
+        <button className="createToken" onClick = {createStorage}> Create Storage</button><br/>
+        <button className="createToken" onClick = {fileShare}> Share File</button><br/>
+        <h1>{loginStatus}</h1>
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -23,6 +23,32 @@ app.post('/register', (req,res)=> {
     (err,result) => {console.log(err);}
     );*/
 })
+
+app.post('/login', (req,res)=> {
+    const username = req.body.username
+    const password = req.body.password
+
+    console.log(username)
+    console.log(password)
+
+    db.query("SELECT * FROM users username = ? and password = ?", [username, password], 
+    (err,result) => {
+        if(err){
+            console.log(err);
+            res.send({err: err})
+
+        }
+            
+        else{
+            if (result.length>0){
+                res.send(result);
+            } else {
+                res.send({message: "Wrong username or password"});
+            }
+        }
+    }
+    );
+})
 const PORT=process.env.PORT || 5000;
 
 app.listen(PORT, ()=>{
